bbef7fdf0efca142408b832c14905df189732067
Tye Stanley <tyestanley090@gmail.com>
2022-08-26 16:27:38 -0700

    updated loginForm and signUpForm

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,7 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-const tyepDefs = gql`
+const typeDefs = gql`
   type User {
     _id: ID
     username: String
